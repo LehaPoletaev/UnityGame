7165f95f1eb1282e738ea9aab66439b5b9584383
Aleksey Poletaev <poletaevalex2001@gmail.com>
2020-10-19 20:00:28 +0700

    Init
    Init project. Add to simple character movement and animation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float moveH, moveV;
+    [SerializeField] private float moveSpead = 1.0f;
+
+   
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+    void FixedUpdate()
+    {
+        moveH = Input.GetAxis("Horizontal") * moveSpead;
+        moveV = Input.GetAxis("Vertical") * moveSpead;
+        rb.velocity = new Vector2(moveH, moveV);
+
+        Vector2 direction = new Vector2(moveH, moveV);
+
+        FindObjectOfType<PlayerAnimation>().setDirection(direction);
+    }
+
+}
